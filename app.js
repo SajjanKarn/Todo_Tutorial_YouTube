bd4f6dd10761296a38f7052b990bf575cbad94f0
ManishKarn56 <69583236+ManishKarn56@users.noreply.github.com>
2020-10-02 10:48:27 +0545

    Update app.js

--- app.js
+++ app.js
@@ -9,15 +9,18 @@ mongoose.connect("mongodb://localhost/todo_express", {
   useUnifiedTopology: true,
 });
 
+
 // middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 
+
 // routes
 app.use(require("./routes/index"))
 app.use(require("./routes/todo"))
+
 
 // server configurations....
 app.listen(3000, () => console.log("Server started listening on port: 3000"));
